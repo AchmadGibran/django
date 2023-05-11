066e97194568e2e5eb84088b907f44a35c854e93
AchmadGibran <achmadgibran27@gmail.com>
2023-05-11 11:56:32 +0800

    update

--- webgibran/blog/views.py
+++ webgibran/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
 
 # Create your views here.
 from .models import Artikel
@@ -9,3 +10,7 @@ def blog_index(request):
         'post' : post
     }
     return render(request, 'index.html', context)
+
+class BlogClass(ListView):
+    model = Artikel
+    template_name = 'index1.html'
